ccfa496460705e3daf715a3afe1ec48a27685dd2
Nathan Smith <nathan@praisetopia.org>
2014-08-01 23:58:50 -0500

    forgot to commit parm.py. doh

--- .parm/parm.py
+++ .parm/parm.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import argparse
+from ParmAnalyzer import ParmAnalyzer
+from ParmParser import ParmParser
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-v', '--verbose', help='Make output verbose', action='store_true')
+parser.add_argument('-v', '--verbose', help='Make output verbose', action='store_true', default=False)
 args = parser.parse_args()
 
-pass
+parm_analyzer = ParmAnalyzer(args.verbose)
+parm_analyzer.update_manifest()
+parm_parser = ParmParser(args.verbose)
+parm_parser.parse_manifest()
