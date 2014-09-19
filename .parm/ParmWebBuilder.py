065ecd0579281567396c7fd0bd2d4b85b9cd828d
Nathan Smith <nathan@praisetopia.org>
2014-09-18 23:13:21 -0500

    removed old files

--- .parm/ParmWebBuilder.py
+++
@@ -1,8 +0,0 @@
-from ParmLogger import ParmLogger
-from ParmOptions import ParmOptions
-import os.path
-
-class ParmWebBuilder:
-	"""Given the generated html, places the html into the appropriate template in the right place"""
-	def __init__(self, template, content, verbose):
-		
