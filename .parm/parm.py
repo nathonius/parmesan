1619115835f82ce45b275d11d76d3d73cdca0365
Nathan Smith <nathan@praisetopia.org>
2014-09-18 23:11:58 -0500

    new simple version

--- .parm/parm.py
+++ .parm/parm.py
@@ -2,6 +2,7 @@
 import argparse
 from ParmManifestGenerator import ParmManifestGenerator as generator
 from ParmManifestParser import ParmManifestParser as parser
+from ParmContentGenerator import ParmContentGenerator as content_generator
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('-v', '--verbose', help='Make output verbose', action='store_true', default=False)
@@ -10,4 +11,6 @@ args = argparser.parse_args()
 parm_generator = generator(args.verbose)
 parm_generator.update_manifest()
 parm_parser = parser(args.verbose)
-parm_parser.parse_manifest()
+paths = parm_parser.parse_manifest()
+content_generator = content_generator(args.verbose)
+content_generator.generate(paths)
