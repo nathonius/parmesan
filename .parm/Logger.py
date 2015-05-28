cb290840a4104fbb294aadb396b8ddb137a1474f
OfficerHalf <nathan@praisetopia.org>
2015-05-28 16:40:41 -0500

    First pyCharm commit? Moved new logger to its own file.

---
+++ .parm/Logger.py
@@ -0,0 +1,42 @@
+import time
+import os.path as path
+
+
+class Logger:
+    """Logger class used to remember log paths and verbosity"""
+
+    @staticmethod
+    def timestamp():
+        """Return the time in the format [yy-mm-dd hh:mm:ss]"""
+        form = '[%y-%m-%d %H:%M:%S]'
+        timestamp = time.strftime(form)
+        return timestamp
+
+    def __init__(self, parameters):
+        self.parameters = parameters
+        self.verbose = parameters["verbose"]
+        log_path = self.get_log_path()
+        self.log_file = open(log_path, 'a')
+        self.log("Logger initialized.")
+
+    def __del__(self):
+        """Make sure to close the log file!"""
+        self.log_file.close()
+
+    def log(self, message):
+        """Write the message to the log file, print if verbose."""
+        message = self.prepare_message(message)
+        self.log_file.write(message)
+        if self.verbose:
+            print(message)
+
+    def get_log_path(self):
+        """Determines the path to the current logfile"""
+        log_path = path.join(self.parameters["parm_path"], "logs")
+        log_timestamp = time.strftime('%y-%m-%d')
+        log_path = path.join(log_path, "log-" + log_timestamp + ".txt")
+        return log_path
+
+    def prepare_message(self, message):
+        """Adds timestamp and strips whitespace from message"""
+        return self.timestamp() + ' ' + message.strip('\n')
--- .parm/parm.py
+++ .parm/parm.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python3
 import argparse
-from ParmManifestGenerator import ParmManifestGenerator as generator
-from ParmManifestParser import ParmManifestParser as parser
-from ParmContentGenerator import ParmContentGenerator as content_generator
-from ParmWriter import ParmWriter as writer
-from ParmLogger import ParmLogger
-from ParmOptions import ParmOptions
+import json
+import os.path as path
+from Logger import Logger
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument('-v', '--verbose', help='Make output verbose', action='store_true', default=False)
-args = argparser.parse_args()
 
-logger = ParmLogger(args.verbose)
-options = ParmOptions(args.verbose)
-parm_generator = generator(logger, options)
-parm_generator.update_manifest()
-parm_parser = parser(logger, options)
-paths = parm_parser.parse_manifest()
-print(paths)
-content_generator = content_generator(logger, options)
-content = content_generator.generate(paths)
-parm_writer = writer(logger, options)
-parm_writer.write(content)
+def main():
+    arg_parser = argparse.ArgumentParser()
+    # All arguments can be set in parm-settings.cfg, but these take priority
+    arg_parser.add_argument('-v', '--verbose', help='Make output verbose', action='store_true', default=False)
+    args = arg_parser.parse_args()
+
+    # Read options and args into parameters
+    parameters = get_parameters(args)
+    # Create logger
+    logger = Logger(parameters)
+    # Read Manifest
+
+
+def get_parameters(cli_args):
+    """cli_args is a Namespace object, so just a blank class. Running vars(args) returns a
+    dictionary of attributes and values. The goal of this function is to read in options
+    from the config file and command line, with cli args taking priority."""
+    cli_args = vars(cli_args)
+    # Get path to config file
+    parm_path = path.dirname(__file__)
+    cfg_path = path.join(parm_path, "parm-settings.cfg")
+    if not path.isfile(cfg_path):
+        raise FileNotFoundError("parm-settings.cfg not found")
+
+    # Open the config file
+    with open(cfg_path, 'r') as fp:
+        try:
+            parameters = json.load(fp)
+        except ValueError:
+            raise ValueError("Could not decode parm-settings.cfg. Check json format.")
+    # Overwrite any params present in both the config file and cli args
+    for value in cli_args.keys():
+        parameters[value] = cli_args[value]
+    # Add the base path of the .parm folder to the parameters, useful in many places
+    parameters["parm_path"] = parm_path
+    return parameters
+
+
+if __name__ == "__main__":
+    main()
