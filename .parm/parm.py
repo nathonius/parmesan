8be0a96e4ef7a3154faf7e80c06ef96bc13e3dfe
U-BENTLEY\Nathan.Smith <Nathan.Smith@Naou1303.bentley.com>
2015-05-28 14:41:33 -0500

    initial version 2 commit

--- .parm/parm.py
+++ .parm/parm.py
@@ -5,18 +5,20 @@ from ParmManifestParser import ParmManifestParser as parser
 from ParmContentGenerator import ParmContentGenerator as content_generator
 from ParmWriter import ParmWriter as writer
 from ParmLogger import ParmLogger
+from ParmOptions import ParmOptions
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('-v', '--verbose', help='Make output verbose', action='store_true', default=False)
 args = argparser.parse_args()
 
 logger = ParmLogger(args.verbose)
-parm_generator = generator(args.verbose)
+options = ParmOptions(args.verbose)
+parm_generator = generator(logger, options)
 parm_generator.update_manifest()
-parm_parser = parser(args.verbose)
+parm_parser = parser(logger, options)
 paths = parm_parser.parse_manifest()
 print(paths)
-content_generator = content_generator(args.verbose)
+content_generator = content_generator(logger, options)
 content = content_generator.generate(paths)
-parm_writer = writer(args.verbose)
+parm_writer = writer(logger, options)
 parm_writer.write(content)
